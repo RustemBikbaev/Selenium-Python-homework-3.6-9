3e3a2bc6ddf0e72beb170fdf6098b347914476a9
RustemBikbaev <ibikbaev97@gmail.com>
2021-08-06 08:59:45 +0300

    Corrected the work of assert. The work is done.

--- test_items.py
+++ test_items.py
@@ -1,6 +1,5 @@
 link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
-
-def test_guest_should_see_login_link(browser):
+def test_there_is_an_add_to_cart_button(browser):
     browser.get(link)
-    browser.find_element_by_css_selector("button.btn.btn-lg.btn-primary.btn-add-to-basket[type='submit']")
+    assert browser.find_elements_by_css_selector("button.btn.btn-lg.btn-primary.btn-add-to-basket[type='submit']"), "No found button to add to cart"
